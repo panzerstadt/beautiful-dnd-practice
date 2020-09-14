243f0a0bbbe7198feacf2bd3d6d3b2d3e814bba2
Tang, Liqun <ts-liqun.tang@rakuten.com>
2020-09-14 11:38:18 +0900

    drag handles instead of the entire thing

--- src/components/Task.js
+++ src/components/Task.js
@@ -5,20 +5,34 @@ import { Draggable } from "react-beautiful-dnd";
 // isDragging: Boolean
 // draggingOver: id / null
 
+const Handle = ({ dragHandleProps, children, ...rest }) => {
+  return (
+    <div
+      className=" bg-orange-300 rounded-md h-4 w-4"
+      {...rest}
+      {...dragHandleProps}
+    >
+      {children}
+    </div>
+  );
+};
+
 const Container = ({ dndProvided, isDragging, children, ...rest }) => {
   const bgColor = isDragging ? "bg-green-200" : "bg-white";
   return (
     <div
       className={
-        "border border-gray-300 mb-2 p-2 rounded-md shadow-sm " + bgColor
+        "flex justify-between items-center border border-gray-300 mb-2 p-2 rounded-md shadow-sm " +
+        bgColor
       }
       {...rest}
       {...dndProvided.draggableProps}
-      {...dndProvided.dragHandleProps}
+      // {...dndProvided.dragHandleProps}
       ref={dndProvided.innerRef}
       isDragging={isDragging}
     >
       {children}
+      <Handle dragHandleProps={dndProvided.dragHandleProps} />
     </div>
   );
 };
